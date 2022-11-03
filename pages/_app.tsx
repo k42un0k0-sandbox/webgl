ae0a107fac126677ad9dc9791553916abb401b7e
k42un0k0 <k42un0k0.put1put1@gmail.com>
2022-11-03 21:35:16 +0900

    move files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Container } from "../modules/app/Container";
+import { Container } from "@/components/Container";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
